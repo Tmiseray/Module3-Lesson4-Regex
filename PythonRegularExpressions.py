d1d0091ab4f1c8aabeab5b0ac1ee9b32e03a71de
Tmiseray <161903165+Tmiseray@users.noreply.github.com>
2024-08-06 16:29:07 -0500

    Adapting the regex pattern

--- PythonRegularExpressions.py
+++ PythonRegularExpressions.py
@@ -20,3 +20,8 @@ Ensure the script still extracts all other valid email addresses.
 
 """
 
+import re
+
+text = "Emails: user1@domain.com, user2@exclude.com, user3@domain.com"
+emails = re.findall(r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-][^exclude]+\.[A-Z|a-z]{2,}\b", text)
+print(emails)
